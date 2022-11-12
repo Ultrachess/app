30967054a1ce1392fc38aa1c89c527a863e0c6f8
glockchain <jesseengerman@hotmail.com>
2022-11-11 23:10:25 -0600

    tournament

---
+++ server/match.py
@@ -0,0 +1,32 @@
+from participant import Participant
+
+class Match:
+    def __init__(self, left, right):
+        #Left and Right participants
+        self.__left = left
+        self.__right = right
+        
+        self.__winner = Participant()
+        self.__loser = Participant()
+
+    def set_winner(self, winner):
+        if winner == self.__left.get():
+            self.__winner.set(winner)
+            self.__loser.set(self.__right.get())
+        elif winner == self.__right.get():
+            self.__winner.set(winner)
+            self.__loser.set(self.__left.get())
+        else:
+            raise Exception("invalid competitor")
+    
+    def get_winner(self):
+        return self.__winner
+    
+    def get_participants(self):
+        return [self.__left, self.__right]
+
+    def can_start(self):
+        left_set = self.__left.get() is not None
+        right_set = self.__right.get() is not None
+        winner_defined = self.__winner.get() is not None
+        return left_set and right_set and not winner_defined
---
+++ server/tournament.py
@@ -0,0 +1,64 @@
+from enum import Enum
+from collections import namedtuple
+
+TournamentTypes = Enum(
+    "TournamentTypes", [
+        "Knockout", 
+        "RoundRobin", 
+        "DoubleRoundRobin", 
+        "Swiss"
+    ]
+)
+
+TournamentOptions = namedtuple(
+    'TournamentOptions', [
+        'type',
+    ]
+)
+
+class Tournament: 
+    def __init__(self, options: TournamentOptions, participants: list<str>):
+        assert len(participants) > 1
+        assert len(participants) % 2 == 0
+        self.type = options.type
+        self.winner_amount = options.winner_amount
+        self.participants = participants
+
+        self.__matches = []
+        self.__rounds = []
+
+        if self.type == TournamentTypes.RoundRobin:
+            self.__gen_round_robin_matches()
+        elif self.type == TournamentTypes.Knockout:
+            self.__gen_knockout_matches()
+    
+    def __gen_round_robin_matches(self):
+        self.__matches = []
+        for i in range(len(self.participants)):
+            for j in range(i + 1, len(self.participants)):
+                self.__matches.append(Match(self.participants[i], self.participants[j]))
+
+    def __gen_knockout_matches(self):
+        new = len(self.__matches) < 1
+        if new:
+            for i in range(len(participants)):
+                self.__matches.append(Match(self.participants[i]), self.participants[i+1])
+                i += 2
+        elif:
+            new_match = []
+            for i in range(len(self.__matches)):
+                winner_left = self.__matches[i].get_winner()
+                winner_right = self.__matches[i+1].get_winner()
+                new_match.append(Match(winner_right, winner_left))
+                i += 2
+            self.__rounds.append(self.__matches)
+            self.__matches = new_match
+
+            println("hello")    
+        
+    def start(self):
+        
+
+
+
+
