e5c22a35926161d1f4d350d4d42ad395c70d51a8
TheExpandingPolytope <jesseengerman@hotmail.com>
2023-03-28 10:39:35 -0500

    inspect time increase

--- front/src/state/game/updater.ts
+++ front/src/state/game/updater.ts
@@ -302,7 +302,7 @@ function useInspect(dispatch) {
   console.log("useInspect");
   const poll = async (dispatch) => {
     console.log("waiting");
-    await delay(5000);
+    await delay(500);
     console.log("done waiting");
     if (!isMountedRef.current) {
       return;
