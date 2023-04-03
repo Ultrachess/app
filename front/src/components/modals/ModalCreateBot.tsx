76b19017858d675761813d8ecce6c8751159fced
TheExpandingPolytope <jesseengerman@hotmail.com>
2023-04-03 13:55:13 -0500

    yarn run build fix

--- front/src/components/modals/ModalCreateBot.tsx
+++ front/src/components/modals/ModalCreateBot.tsx
@@ -216,22 +216,6 @@ const DialogOverlay = styled(Dialog.Overlay, {
   animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
 });
 
-const DialogContent = styled(Dialog.Content, {
-  backgroundColor: "white",
-  borderRadius: 6,
-  boxShadow:
-    "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "90vw",
-  maxWidth: "450px",
-  maxHeight: "85vh",
-  padding: 25,
-  animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
-  "&:focus": { outline: "none" },
-});
 
 const DialogTitle = styled(Dialog.Title, {
   margin: 0,
