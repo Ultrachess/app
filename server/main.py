c5517abe30631c923d397a86d1abd1443834a754
TheExpandingPolytope <jesseengerman@hotmail.com>
2023-05-01 13:53:44 -0500

    main py backend reject inputs

--- server/main.py
+++ server/main.py
@@ -221,17 +221,18 @@ def handle_advance(data):
             except Exception as e:
                 msg = "Payload does not conform to ERC20 deposit ABI"
                 logger.error(f"{msg}\n{traceback.format_exc()}")
-                # return reject_input(msg, data["payload"])
+                return reject_input(msg, data["payload"])
 
             # Check if the header matches the Keccak256-encoded string "ERC20_Transfer"
             input_header = decoded[0]
             if input_header == ERC20_TRANSFER_HEADER:
-                # return reject_input(
-                #     f"Input header is not from an ERC20 transfer", data["payload"]
-                # )
                 depositAddress = decoded[1]
                 depositTokenAddress = decoded[2]
                 depositAmount = decoded[3]
+            else:
+                return reject_input(
+                    f"Input header is not from an ERC20 transfer", data["payload"]
+                )
 
         else:
             # Is user input
